aba4150faedd27dedbaf4e78aca21bee6db4494e
Marlon Cassar <cassar16@gmail.com>
2015-06-25 12:22:04 +0200

    Sixth Commit : Adding the Sound
    After having a fully functional game with all the buttons linked, I
    added the sound to the game, giving very importance to it, since it
    helps to make it more attractive.

---
+++ BALANCE_Development _Stages/Assets/Script/soundCollision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundCollision : MonoBehaviour {
+	public AudioClip Water;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter (Collision col)
+	{
+
+		
+
+		if(col.gameObject.tag == "box1")
+		{
+			
+
+			GetComponent<AudioSource> ().PlayOneShot (Water);
+			
+			
+			
+		}
+	}
+	
+
+}
